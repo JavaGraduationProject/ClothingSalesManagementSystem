3266a6605300cd3d5f92e6ec957ef1258f2041a2
Ganson <1145317236@qq.com>
2024-09-10 12:29:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# 全目录
+
+[更多系统、论文，供君选择 ~~>](https://www.bitwise.net.cn)
+
+# 117.ClothingSalesManagementSystem
+
+<p>群: 983063232(大佬群 2TB学习资料,讲解)(入群获取sql文件)</p>
+
+<p><h1 align="center">117.服装销售管理系统</h1></p>
+
+
+<p align="center">
+	<img src="https://img.shields.io/badge/jdk-1.8-orange.svg"/>
+    <img src="https://img.shields.io/badge/springBoot-5.x-lightgrey.svg"/>
+    <img src="https://img.shields.io/badge/html-5.x-blue.svg"/>
+    <img src="https://img.shields.io/badge/mysql-5.x-yellow.svg"/>
+</p>
+
+# 简介
+
+
+> 本代码来源于网络,仅供学习参考使用,请入群(983063232)后联系群主索要sql文件!
+>
+> 提供1.远程部署/2.修改代码/3.设计文档指导/4.框架代码讲解等服务
+
+>访问地址: http://localhost:28089/index
+> 
+> 13700002703	123456
+> 
+> admin  123456
+
+
+# 环境
+
+- <b>IntelliJ IDEA 2009.3</b>
+
+- <b>Mysql 5.7.26</b>
+
+- <b>Tomcat 7.0.73</b>
+
+- <b>JDK 1.8</b>
+
+
+
+
+## 缩略图
+
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/4f51f339-3df4-4ed9-8d2a-ef02b9588990.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/4f51f339-3df4-4ed9-8d2a-ef02b9588990.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/c2b7b217-c24c-4efe-8eec-5da123afaeeb.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/19c4f2b5-fe7b-4886-9aa3-a43853d56afc.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/bfded71c-8524-4366-b86f-bf088fe31947.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/51ceae69-277b-4665-b72f-20d625dbea57.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/68145275-e007-4294-8943-759bf407296d.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/3373d0a6-cfb1-4854-830b-fe5d9aba9da8.png)
+
+
---
+++ src/main/java/ltd/newbee/mall/controller/common/CommonController.java
@@ -0,0 +1,60 @@
+/**
+ * 严肃声明：
+ * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
+ * 本系统已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
+ * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
+ * Copyright (c) 2019-2020 十三 all rights reserved.
+ * 版权所有，侵权必究！
+ */
+package ltd.newbee.mall.controller.common;
+
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.ShearCaptcha;
+import ltd.newbee.mall.common.Constants;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author 13
+ * @qq交流群 796794009
+ * @email 2449207463@qq.com
+ * @link https://github.com/newbee-ltd
+ */
+@Controller
+public class CommonController {
+
+    @GetMapping("/common/kaptcha")
+    public void defaultKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        httpServletResponse.setHeader("Cache-Control", "no-store");
+        httpServletResponse.setHeader("Pragma", "no-cache");
+        httpServletResponse.setDateHeader("Expires", 0);
+        httpServletResponse.setContentType("image/png");
+
+        ShearCaptcha shearCaptcha= CaptchaUtil.createShearCaptcha(150, 30, 4, 2);
+
+        // 验证码存入session
+        httpServletRequest.getSession().setAttribute("verifyCode", shearCaptcha);
+
+        // 输出图片流
+        shearCaptcha.write(httpServletResponse.getOutputStream());
+    }
+
+    @GetMapping("/common/mall/kaptcha")
+    public void mallKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        httpServletResponse.setHeader("Cache-Control", "no-store");
+        httpServletResponse.setHeader("Pragma", "no-cache");
+        httpServletResponse.setDateHeader("Expires", 0);
+        httpServletResponse.setContentType("image/png");
+
+        ShearCaptcha shearCaptcha= CaptchaUtil.createShearCaptcha(110, 40, 4, 2);
+
+        // 验证码存入session
+        httpServletRequest.getSession().setAttribute(Constants.MALL_VERIFY_CODE_KEY, shearCaptcha);
+
+        // 输出图片流
+        shearCaptcha.write(httpServletResponse.getOutputStream());
+    }
+}
